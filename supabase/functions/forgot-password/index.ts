2a568d9a0e15e9dda11296dfccbc6b0f153d396c
EmiMaldonado <133152366+EmiMaldonado@users.noreply.github.com>
2025-09-22 12:46:47 +0200

    Update index.ts

--- supabase/functions/forgot-password/index.ts
+++ supabase/functions/forgot-password/index.ts
@@ -8,6 +8,7 @@ import { PasswordResetEmail } from './_templates/password-reset-email.tsx';
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
+  "Access-Control-Allow-Methods": "POST, GET, OPTIONS, PUT, DELETE"
 };
 
 interface ForgotPasswordRequest {
